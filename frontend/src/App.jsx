a453162346a7fe906523ff8fb2dd9de98d3e23ef
Jhosett <darwinjhosettbermudezromero@gmail.com>
2026-02-27 08:52:47 -0500

    Primer Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Saludar from './playground/Component'
+
+function App() {
+  return (
+    <>
+     <h1>DARWIN JHOSETT</h1>
+     <Saludar></Saludar>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/playground/Component.jsx
@@ -0,0 +1,18 @@
+function Saludar() {
+    return (
+        <h2>Bienvenido</h2>
+    )
+}
+export default Saludar;
+
+
+
+
+
+// import React from 'react'
+
+// export default function Component() {
+//   return (
+//     <div>Component</div>
+//   )
+// }
