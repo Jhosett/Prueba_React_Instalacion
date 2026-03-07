2e10d76eb7f3634e3c974d19adc80eba9ef084a5
Jhosett <darwinjhosettbermudezromero@gmail.com>
2026-03-07 16:41:26 -0500

    Realizando tabla de Hooks

--- frontend/src/playground/HomeHooks.jsx
+++ frontend/src/playground/HomeHooks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import HookTable from '../components/HookTable';
 
 export default function HomeHooks() {
 
@@ -7,10 +8,12 @@ export default function HomeHooks() {
 
   return (
     <div>
-        <h1>HomeHooks</h1>
+      <h1 className="text-center text-4xl font-bold m-4 text-gray-800">HomeHooks</h1>
+      <h2 className='text-2xl text-center font-bold my-8 text-gray-500'>Ejemplos de Hooks de React</h2>
+      <HookTable/>
 
-        {/* Agregar lo de useNavigate a través de esta pestaña */}
-        <h2 onClick={() => navigation('/useState')}>Ir a UseState</h2>
+      {/* Agregar lo de useNavigate a través de esta pestaña */}
+      {/* <h2 className="text-2xl cursor-pointer" onClick={() => navigation('/useState')}>Ir a UseState</h2> */}
 
 
     </div>
